9d04b335e5494ad41d71a5fe7348652d02853475
spiritman90 <phemmyadey@gmail.com>
2023-06-19 16:48:34 +0100

    Added the state variables to display and hide form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import FormSplitBill from "./components/FormSplitBill";
 import Button from "./components/Button";
 import FormAddFriend from "./components/FormAddFriend";
 import FriendsList from "./components/FriendsList";
+import { useState } from "react";
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((prev) => !prev);
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
