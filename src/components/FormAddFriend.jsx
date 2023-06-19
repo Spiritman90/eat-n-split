f0b593a1612f54cac4e9dfe54c8aba43cd463880
spiritman90 <phemmyadey@gmail.com>
2023-06-19 17:25:41 +0100

    worked on adding a new friend

--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
-export default function () {
+export default function ({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) {
+      alert("Please add a name and image");
+      return;
+    }
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    onAddFriend(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👬 Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🌄 Image URL</label>
-      <input type="text" />
+      <input type="text" value={image} onChange={(e) => e.target.value} />
       <Button>Add</Button>
     </form>
   );
